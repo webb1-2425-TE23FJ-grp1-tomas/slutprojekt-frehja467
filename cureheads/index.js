966175ebf1bbe1e556f10870b8a8d1263b91f20f
frehja467 <frehja467@edu.linkoping.se>
2025-05-15 11:01:08 +0000

    aa

--- cureheads/index.js
+++ cureheads/index.js
@@ -4,3 +4,8 @@ const wishinfo = document.querySelector('.wishinfo');
 btn.addEventListener("click", function(event){
     wishinfo.classList.toggle('hidden');
 })
+const disinfo = document.querySelector('.disinfo');
+
+btn.addEventListener("click", function(event){
+    disinfo.classList.remove('hidden');
+})
